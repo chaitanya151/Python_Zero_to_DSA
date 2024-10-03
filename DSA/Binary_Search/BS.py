cc88ff036116a2aa83961cd58b630ca6138e2b48
unknown <chaitanyadeshmukh7@gmail.com>
2024-10-03 23:24:45 +0530

    Interview.txt

--- DSA/Binary_Search/BS.py
+++ DSA/Binary_Search/BS.py
@@ -4,7 +4,6 @@ target = 6
 
 
 def binary_search(num, low, high):
-    n = len(num)
     while low <= high:
         mid = (low + high) // 2
         if num[mid] == target:
---
+++ Data_Python_Questions/P_06.py
@@ -0,0 +1,5 @@
+"""
+Yash Technologies
+Smallest Positive Integer that 
+cannot be represented as the sum of any subset of the array.
+"""
